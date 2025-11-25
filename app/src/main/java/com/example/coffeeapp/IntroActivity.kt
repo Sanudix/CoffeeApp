5fece59e7419c7fecc68c47d7d5b61f83a6eec19
sanudix <nikitasochi001@gmail.com>
2025-11-25 18:07:27 +0300

    add IntroScreen UI and Activity

---
+++ app/src/main/java/com/example/coffeeapp/IntroActivity.kt
@@ -0,0 +1,20 @@
+package com.example.coffeeapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.coffeeapp.ui.feature.IntroScreen
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            IntroScreen {
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+        }
+    }
+}
