960c0eacd3aac1fe5917d750d673700e9c00be80
sanudix <nikitasochi001@gmail.com>
2025-11-28 11:06:52 +0300

    edit CategorySection import

--- app/src/main/java/com/example/coffeeapp/ui/feature/home/CategorySection.kt
+++ app/src/main/java/com/example/coffeeapp/ui/feature/home/CategorySection.kt
@@ -1,33 +1,12 @@
 package com.example.coffeeapp.ui.feature.home
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.coffeeapp.R
 
 @Composable
 @Preview
--- app/src/main/java/com/example/coffeeapp/ui/feature/home/CategorySectionItems.kt
+++ app/src/main/java/com/example/coffeeapp/ui/feature/home/CategorySectionItems.kt
@@ -2,10 +2,7 @@ package com.example.coffeeapp.ui.feature.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
